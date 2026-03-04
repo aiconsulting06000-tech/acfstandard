cbbd8ac542006d4ed31959e5183a511d1e4c54db
aiconsulting06000-tech <aiconsulting06000@gmail.com>
2026-03-04 22:51:35 +0100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -566,7 +566,7 @@ footer{background:var(--navy2);border-top:1px solid var(--bd);padding:50px 0 28p
     <div class="nr">
       <div class="nlm">
         <a href="/en/acf-score">ACF Score</a>
-        <a href="/standard">Standard</a>
+        <a href="/standard">The Standard</a>
         <a href="/control">ACF Control</a>
         <a href="/certification">ACF Certification</a>
       </div>
