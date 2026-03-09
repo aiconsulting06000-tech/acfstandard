f616425774baac70d723c2f2a2cc8ed51f4c0478
aiconsulting06000-tech <aiconsulting06000@gmail.com>
2026-03-09 18:26:08 +0100

    Create request.ts

---
+++ i18n/request.ts
@@ -0,0 +1,17 @@
+import { getRequestConfig } from 'next-intl/server'
+import { notFound } from 'next/navigation'
+
+const locales = ['fr', 'en', 'es', 'de', 'it', 'pt', 'nl', 'pl', 'sv', 'da', 'no', 'fi', 'is', 'et', 'lv', 'lt', 'cs', 'sk', 'hu', 'ro', 'bg', 'hr', 'sl', 'sr', 'uk', 'el', 'ar', 'he', 'tr', 'ja', 'zh', 'ko', 'id', 'ms', 'th', 'vi', 'hi', 'ru']
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  let locale = await requestLocale
+  
+  if (!locale || !locales.includes(locale)) {
+    locale = 'fr'
+  }
+
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default
+  }
+})
